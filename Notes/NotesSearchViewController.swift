13620d1b68d8a7a77970f73b7acaed46d1c3cc6b
Steve Cox <coxsteve@gmail.com>
2016-05-20 11:04:25 -0600

    starting UISearchBar attempt

---
+++ Notes/NotesSearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotesSearchViewController.swift
+//  Notes
+//
+//  Created by Steve Cox on 5/20/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class NotesSearchViewController: UISearchBar {
+
+}
